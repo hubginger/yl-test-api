fbb6f939400edaefe46bde1fc82ee382e1764abf
姜通 <jiangtong@yljt.cn>
2024-01-03 17:29:40 +0800

    delete alure results

---
+++ test_cases/test_ent/conftest.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+"""
+
+    前后置
+    参数化
+
+"""
+
+# @Time    :  2024-01-03 16:48:43
+# @Author  :  jiangtong
+# @Email   :  gingerqgyy@outlook.com
+# @Project :  yl_test_api
+# @File    :  conftest
